34dda7a5cb16ba74cf91a4a9bb058a82078d9a44
Destiny <dejenkins@justworks.com>
2024-08-10 15:19:52 -0400

    added pool function to connect Postgress to env

--- index.js
+++ index.js
@@ -2,3 +2,14 @@
 const inquirer = require("inquirer");
 const { Pool } = require('pg');
 require("dotenv").config(); 
+
+// Pool instance for PostgreSQL database connection using environment variables
+const pool = new Pool(
+    {
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        host: 'localhost',
+        database: process.env.DB_NAME,
+    },
+    console.log(`Connected to the store_db Database!`) // console.log a message to confirm successful connection
+);
