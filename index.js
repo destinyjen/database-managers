41490d717287cafd223aa3bcb7fb7985156f9dd1
Destiny <dejenkins@justworks.com>
2024-08-10 15:13:33 -0400

    added in required modules to index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+// Import required modules (inquier for user prompts, pg for PostgreSQL database interaction, dotenv for loading environment variables)
+const inquirer = require("inquirer");
+const { Pool } = require('pg');
+require("dotenv").config(); 
